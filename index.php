189449a8eb511166a926a9cd767ca5248ab9d7ff
Andrés Salazar <salazarcode@gmail.com>
2018-12-13 14:39:55 -0500

    Debería guardarse

--- index.php
+++ index.php
@@ -5,3 +5,5 @@ $name = $_GET["name"];
 echo "Hola " . $name;
 
 //Aparentemente un comentario que sólo estará en la rama primer_mejora
+
+//Le llamo tercera mejora pero es la segunda, igual sirve para probar.
